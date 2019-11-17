7bc663f38ab48e75d015c76c67b524a53ca1c07e
Sofyan Sugianto <sofyanartem@gmail.com>
2019-11-17 20:51:46 +0700

    add helper on initial install

---
+++ pythonpath/ThemeChanger/Helper.py
@@ -0,0 +1,16 @@
+# Setup intro
+def prepare_new_install():
+    # copy intro.png ke root/lotc/active-theme/intro.png
+    # backup intro.png
+    # symlink root/lotc/active-theme/intro.png ke root/program/intro.png
+
+    # setup sofficcerc
+    # backup sofficerc in root install dir
+    # copy sofficerc owned by lotc replacing sofficerc above
+
+
+    # setup personas dir
+    # copy personas dir to root/lotc/active-themes/personas
+    # backup personas directory
+    # symlink lotc/active-themes/personas to root/share/gallery/personas
+    pass
