57ed34e98a165f21089e8a5c4213538f7bea3cc9
Sofyan Sugianto <sofyanartem@gmail.com>
2019-11-20 13:30:56 +0700

    fix superuser privileges on new install

--- pythonpath/ThemeChanger/Helper.py
+++ pythonpath/ThemeChanger/Helper.py
@@ -1,20 +1,15 @@
 import os
-from shutil import copyfile
+from shutil import copyfile, copytree
 import sys
 import uno
+import subprocess
 import unohelper
 import traceback
+# from elevate import elevate
 
 # Setup intro
-def prepare_new_install(ctx, theme_name="default-libreoffice"):
-    if sys.platform == "linux":
-
-        pass
-    elif sys.platform == "windows":
-        pass
-    else:
-        pass
-    return
+def prepare_new_install(ctx):
+    theme_name = "default-libreoffice"
     # get program install dir
     ps = ctx.getServiceManager().createInstanceWithContext('com.sun.star.util.PathSubstitution', ctx)
     instdir = uno.fileUrlToSystemPath(ps.getSubstituteVariableValue("$(instpath)"))
@@ -27,17 +22,23 @@ def prepare_new_install(ctx, theme_name="default-libreoffice"):
 
     prefered_themedir = "%s/%s" % (lotcdir, theme_name)
 
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
     if os.path.exists(userdir + "/lotc-prepare"):
         return
 
+    if not os.path.exists(personas_userdir):
+        copytree(personas_sysdir, personas_userdir)
+
     if not os.path.exists(prefered_themedir):
-        os.makedirs(prefered_themedir)
-        copyfile(program_sysdir + "/intro.png", prefered_themedir + "/intro.png")
-        copyfile(program_sysdir + "/sofficerc", prefered_themedir + "/sofficerc")
+        os.makedirs(prefered_themedir + "/program")
+        os.makedirs(prefered_themedir + "/personas")
+        copyfile(program_sysdir + "/intro.png", prefered_themedir + "/program/intro.png")
+        copyfile(program_sysdir + "/sofficerc", prefered_themedir + "/program/sofficerc")
 
 
     if os.path.exists(lotcdir + "/active-theme"):
-        os.remove(lotcdir + "/active-themes")
+        os.remove(lotcdir + "/active-theme")
 
     # create symlink to active theme.
     # it will be live at (as symlink):
@@ -47,42 +48,70 @@ def prepare_new_install(ctx, theme_name="default-libreoffice"):
     except Exception as e:
         print(e)
         traceback.print_exc()
+    active_dir = lotcdir + "/active-theme"
+    RUN_ME = "import os, sys; " \
+             "os.chdir('{}');" \
+             "from Helper import *;" \
+             "setup_intro_image('{}', '{}');" \
+             "setup_sofficerc('{}', '{}');" \
+             "setup_personas('{}', '{}');".format(current_dir, program_sysdir, active_dir, program_sysdir, active_dir,
+                                                  personas_sysdir, personas_userdir)
 
-    # backup intro.png
-    # TODO check user privilege (because using root privileges to do this section)
-    if not os.path.islink(program_sysdir + "/intro.png"):
-        try:
-            os.rename(program_sysdir + "/intro.png", program_sysdir + "/intro.png.orig")
-            os.symlink(prefered_themedir, program_sysdir + "/intro.png")
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
+    if sys.platform.startswith("win"):
+        pass
+    else:
+        # run as Administrator
+        sudo = "sudo"
+        if sys.platform.startswith("darwin"):
+            sudo = "osascript"
+            subprocess.call(["sudo", sudo, "python3", "-c", RUN_ME] + sys.argv)
 
-    # setup sofficcerc
-    # TODO check user privilege (because using root privileges to do below section)
-    if os.path.exists(program_sysdir + "/sofficerc"):
-        try:
-            os.rename(program_sysdir + "/sofficerc", program_sysdir + "/sofficerc.orig")
-            os.symlink(prefered_themedir, program_sysdir + "/sofficerc")
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
-
-
-    # setup personas dir
-    # TODO check user privilege (because using root privileges to do below section)
-    if os.path.exists(personas_sysdir) and not os.path.islink(personas_sysdir):
-        try:
-            copyfile(personas_sysdir, personas_userdir)
-            os.rename(personas_sysdir, personas_sysdir + ".orig")
-            os.symlink(personas_userdir, personas_sysdir)
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
-
+        if sys.platform.startswith("linux"):
+            if os.environ.get("DISPLAY"):
+                prompts = ["pkexec","gksudo", "kdesudo"]
+                for item in prompts:
+                    a = os.system("which %s" % item)
+                    if a == 0:
+                        sudo = item
+                        break
+            subprocess.call([sudo, sys.executable, "-c", RUN_ME] + sys.argv)
+        pass
     # write config that preparation completed
     with open(userdir + "/lotc-prepare", "w") as f:
         f.write("finished")
         f.close()
+    return
 
-    pass
+
+def setup_intro_image(program_sysdir, prefered_themedir):
+    # backup intro.png
+    if not os.path.islink(program_sysdir + "/intro.png"):
+        try:
+            os.rename(program_sysdir + "/intro.png", program_sysdir + "/intro.png.orig")
+            os.symlink(prefered_themedir + "/program/intro.png", program_sysdir + "/intro.png")
+            print("intro.png preparation success")
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+
+def setup_sofficerc(program_sysdir, prefered_themedir):
+    # setup sofficcerc
+    if os.path.exists(program_sysdir + "/sofficerc"):
+        try:
+            os.rename(program_sysdir + "/sofficerc", program_sysdir + "/sofficerc.orig")
+            os.symlink(prefered_themedir + "/program/sofficerc", program_sysdir + "/sofficerc")
+            print("sofficerc preparation success")
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+
+def setup_personas(personas_sysdir, personas_userdir):
+    # setup personas dir
+    if os.path.exists(personas_sysdir) and not os.path.islink(personas_sysdir):
+        try:
+            os.rename(personas_sysdir, personas_sysdir + ".orig")
+            os.symlink(personas_userdir, personas_sysdir)
+            print("personas lists preparation success")
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
